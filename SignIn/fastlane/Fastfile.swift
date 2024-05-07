524aa4c817cd6208a8d9395a5b11f0eddd20abcb
BKiOS <168804132+BKiOS@users.noreply.github.com>
2024-05-07 18:43:30 +0530

    Add files via upload

---
+++ SignIn/fastlane/Fastfile.swift
@@ -0,0 +1,63 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+	func betaLane() {
+	desc("Push a new beta build to TestFlight")
+		incrementBuildNumber(xcodeproj: "SignIn.xcodeproj")
+        package(config: Staging())
+//		uploadToTestflight(username: "boobalan.m@spantechnologyservices.com")
+	}
+
+    func package(config: Configuration) {
+        var export: [String: Any]? = [
+            "signingStyle": "manual",
+            "provisioningProfiles": [appIdentifier: "DemoTest"],
+            "signingCertificate": "F743FEF92ED488DD77C73D696538656C9D156FEB"
+        ]
+
+        buildApp(workspace: "SignIn.xcworkspace", scheme: "SignIn", clean: true, exportMethod: "development", exportOptions: .userDefined(export))
+    }
+    
+    
+}
+
+protocol Configuration {
+    /// file name of the certificate
+    var certificate: String { get }
+
+    /// file name of the provisioning profile
+    var provisioningProfile: String { get }
+
+    /// configuration name in xcode project
+    var buildConfiguration: String { get }
+
+    /// the app id for this configuration
+    var appIdentifier: String { get }
+
+    /// export methods, such as "ad-doc" or "appstore"
+    var exportMethod: String { get }
+}
+
+struct Staging: Configuration {
+    var certificate = "development"
+    var provisioningProfile = "DemoTest"
+    var buildConfiguration = "Staging"
+    var appIdentifier = "com.newProject.SignIn"
+    var exportMethod = "development"
+}
+
+struct Production: Configuration {
+    var certificate = "ios_distribution"
+    var provisioningProfile = "DemoTest"
+    var buildConfiguration = "Production"
+    var appIdentifier = "com.newProject.SignIn"
+    var exportMethod = "ad-hoc"
+}
