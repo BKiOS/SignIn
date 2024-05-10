8d13d04fb259015aa9edb50c9093f3ef3b29eed5
BKiOS <168804132+BKiOS@users.noreply.github.com>
2024-05-10 18:20:58 +0530

    Delete SignIn/fastlane directory

--- SignIn/fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "com.newProject.SignIn" } // The bundle identifier of your app
-var appleID: String { return "boobalan.m@spantechnologyservices.com" } // Your Apple Developer Portal username
-
-var itcTeam: String? { return "1791526" } // App Store Connect Team ID
-var teamID: String { return "X372X3URRM" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
